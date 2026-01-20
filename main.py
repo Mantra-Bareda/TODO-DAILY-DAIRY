f006e61a12702956a5171f3ce0afee452effd13a
Mantra Bareda <baredamantra@gmail.com>
2026-01-20 19:36:32 +0530

    Some Changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-## BASIC IMPORTS
 import encrypter as enc
 import customtkinter as ctk
 import os
@@ -9,184 +8,156 @@ import shutil
 import tkinter as tk
 from datetime import date
 from tkinter import filedialog
+import sys
 
 
-## FUNCTIONS
+APP_NAME = "diary_planner"
 
-## 1. CHECKING FOLDER (DATA FOLDER)
+def get_data_dir():
+    base = os.getenv("APPDATA")
+    path = os.path.join(base, APP_NAME)
+    os.makedirs(path, exist_ok=True)
+    return path
+
+DATA_DIR = get_data_dir()
+DIARY_DIR = os.path.join(DATA_DIR, "diary_entries")
+TASK_DIR = os.path.join(DATA_DIR, "tasks")
+CONFIG_FILE = os.path.join(DATA_DIR, "config.txt")
+
+os.makedirs(DIARY_DIR, exist_ok=True)
+os.makedirs(TASK_DIR, exist_ok=True)
+
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
 def check_folders():
-    paths = ["data\\diary_entries" , "data\\tasks"]
-    for i in paths:
-        if not os.path.exists(i):
-            os.mkdir(i)
-        else:pass          
+    os.makedirs(DIARY_DIR, exist_ok=True)
+    os.makedirs(TASK_DIR, exist_ok=True)
 
-
-## 2. FUNCTION TO CHECK PASSWORD IS CORRECT OR NOT
 def login_check():
     global CHECK_RESULT
     CHECK_RESULT = ""
-    check= os.path.isfile("data\config.txt")
-    if check:
-        with open("data\config.txt" , "r") as f:
+
+    if os.path.isfile(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as f:
             p = enc.decrypt(f.read())
-            if str(pw.get()) == p:
-                CHECK_RESULT= True
-            else:
-                CHECK_RESULT= False
+            CHECK_RESULT = str(pw.get()) == p
     else:
-        if os.path.isdir("data"):
-            shutil.rmtree("data")
-            os.makedirs("data")
-            with open("data\config.txt" , "w") as f:
-                f.write(enc.encrypt(pw.get()))
-        else:
-            os.makedirs("data")
-            with open("data\config.txt" , "w") as f:
-                f.write(enc.encrypt(pw.get()))
-            
+        with open(CONFIG_FILE, "w") as f:
+            f.write(enc.encrypt(pw.get()))
+        CHECK_RESULT = True
+
     win.destroy()
 
-## 3. CHANGING PASSWORD 2 FUNCTIONS
 def change_pass1():
-    new_pass_entry.place(x = 250 , y = 250)
-    bt5.place(x = 250 , y = 275)
+    new_pass_entry.place(x=250, y=250)
+    bt5.place(x=250, y=275)
 
 def change_pass2():
-    with open("data\config.txt" , "w") as f:
-                f.write(enc.encrypt(new_pass_entry.get()))
-    
+    with open(CONFIG_FILE, "w") as f:
+        f.write(enc.encrypt(new_pass_entry.get()))
+
     new_pass_entry.place_forget()
     bt5.place_forget()
 
-
-
-## 4. TAKE BACKUP OF EVERYTHING
 def make_backup():
     a = select_folder()
-    folder_path = 'data'
+    if not a:
+        return
 
-    zip_file_path = str(str(a) + "/" + f'data_{date.today()}.zip')
+    zip_file_path = os.path.join(a, f'data_{date.today()}')
+    shutil.make_archive(zip_file_path, 'zip', DATA_DIR)
 
-    shutil.make_archive(zip_file_path, 'zip', folder_path)
+    Label_backup_done.place(x=250, y=200)
 
-    Label_backup_done.place(x = 250 , y = 200)    
-
-
-## 5. SELECTING BACKUP FOLDER
 def select_folder():
     root = tk.Tk()
-    root.withdraw()  
-    folder_selected = filedialog.askdirectory(initialdir="G:/My Drive/backup/mantra/some text file and extra/text/personal")
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
     return folder_selected
 
-
-## 6. BIND ENTER KEY TO RUN LOGIN CHECK FUNTION
-def on_enter(event):
-    login_check()
-
-
-## 7. LOGIN FUNCTION (CREATE LOGIN WINDOW AND ALL)
-def login():
-    global win , pw
-    check_folders()
-    win = ctk.CTk()
-    win.title("Login Window")
-    win.geometry("500x500+100+100")
-    win.resizable(False , False)
-    win.configure(bg_color = "#303633")
-    win.iconbitmap("icon.ico")
-    
-    label = ctk.CTkLabel(win , text = "Login Window" ,font=("Times New Roman", 40) , text_color= "#7FB069")
-    label.place(x= 125 , y = 200)
-
-    win.bind("<Return>", on_enter)
-
-    pw_label = ctk.CTkLabel(win ,font=("Times New Roman", 16) , text_color="#7FB069", text = "Enter Password : ")
-    pw_label.place(x = 125, y = 300)
-    pw = ctk.CTkEntry(win , show = "*",width=150 , text_color="#78C3FB")
-    pw.place(x=230,y=300)
-
-    pw.after(100, pw.focus_set)
-    
-    check_bt = ctk.CTkButton(win , text = "Check",font=("Times New Roman", 16) , command=lambda : [login_check()])
-    add_hover_effects(check_bt)
-    check_bt.place(x=200 , y = 350)
-
-    win.mainloop()
-
-## 8. FUNTION TO ADD A GOOD HOVER EFFECT TO THE BUTTONS THAT ARE PASSED IN FUNCTIONS
-def add_hover_effects(button, hover_color="#78C3FB", normal_color="#1F538D" , Text_colour_hover = "#303633" , normal_text = "#CCF5AC"):
+def add_hover_effects(button, hover_color="#78C3FB", normal_color="#1F538D",Text_colour_hover="#303633", normal_text="#CCF5AC"):
     def on_hover(event):
-        button.configure(fg_color=hover_color , text_color= Text_colour_hover)
+        button.configure(fg_color=hover_color, text_color=Text_colour_hover)
 
     def on_leave(event):
-        button.configure(fg_color=normal_color, text_color = normal_text)
+        button.configure(fg_color=normal_color, text_color=normal_text)
 
     button.bind("<Enter>", on_hover)
     button.bind("<Leave>", on_leave)
 
+def on_enter(event):
+    login_check()
 
+def login():
+    global win, pw
+    check_folders()
 
-## 9. MAIN FUNCTION WITH ALL UI AND OTHER THINGS AND MANAGAGE ALL OTHER FUNTIONS
+    win = ctk.CTk()
+    win.title("Login Window")
+    win.geometry("500x500+100+100")
+    win.resizable(False, False)
+    win.configure(bg_color="#303633")
+
+    label = ctk.CTkLabel(win, text="Login Window",font=("Times New Roman", 40),text_color="#7FB069")
+    label.place(x=125, y=200)
+
+    win.bind("<Return>", on_enter)
+
+    pw_label = ctk.CTkLabel(win, font=("Times New Roman", 16),text_color="#7FB069",text="Enter Password : ")
+    pw_label.place(x=125, y=300)
+
+    pw = ctk.CTkEntry(win, show="*", width=150, text_color="#78C3FB")
+    pw.place(x=230, y=300)
+    pw.after(100, pw.focus_set)
+
+    check_bt = ctk.CTkButton(win, text="Check",font=("Times New Roman", 16),command=login_check)
+    add_hover_effects(check_bt)
+    check_bt.place(x=200, y=350)
+
+    win.mainloop()
+
 def main():
-    global Label_backup_done , new_pass_entry , bt5
+    global Label_backup_done, new_pass_entry, bt5
 
-    ## RUN LOGIN FUNCTION
     login()
-
-    ## GET IF PROPER CREDENTIALS
     a = CHECK_RESULT
 
-    ## PROCEED AS PER RESULT
-    if a == False:
+    if a is False:
         messagebox.showinfo("Wrong Credentials")
-    else:
-        
-        ## IF PROPER CREDENTIAL --> RUN FUNCTIONS
-        
-        ## CREATE A CUSTOM-TKINTER WINDOWS
-        root = ctk.CTk()
-        root.geometry("500x500+50+50")
-        root.resizable(False,False)
-        root.title("Multi-Things")
-        root.iconbitmap("icon.ico")
+        return
 
+    root = ctk.CTk()
+    root.geometry("500x500+50+50")
+    root.resizable(False, False)
+    root.title("Multi-Things")
 
-        ## CREATE BUTTON -> 1 - DIARY , 2 - TASK , 3 - BACKUP , 4 - CHANGE_PASS , 5 - CHANGE_CONFIRM
-        bt1 = ctk.CTkButton(root , text = "Daily Diary" ,font=("Times New Roman", 16),text_color="#CCF5AC", command=lambda:[main_diary.main()])
+    bt1 = ctk.CTkButton(root, text="Daily Diary",font=("Times New Roman", 16),text_color="#CCF5AC",command=main_diary.main)
 
-        bt2 = ctk.CTkButton(root , text = "To-do list",font=("Times New Roman", 16),text_color="#CCF5AC", command=lambda:[main_task.main()])
+    bt1.place(x=100, y=100)
 
-        bt1.place(x=100 , y =100)
+    bt2 = ctk.CTkButton(root, text="To-Do-List",font=("Times New Roman", 16),text_color="#CCF5AC",command=main_task.main)
+    bt2.place(x=100, y=150)
 
-        bt2.place(x=100 , y =150)
+    bt3 = ctk.CTkButton(root, text="Backup",font=("Times New Roman", 16),text_color="#CCF5AC",command=make_backup)
+    bt3.place(x=100, y=200)
 
-        bt3 = ctk.CTkButton(root , text = "Backup" ,font=("Times New Roman", 16),text_color="#CCF5AC", command=lambda : [make_backup()])
+    bt4 = ctk.CTkButton(root, text="Change Password",font=("Times New Roman", 16),text_color="#CCF5AC",command=change_pass1)
+    bt4.place(x=100, y=250)
 
-        bt3.place(x = 100 , y = 200)
+    new_pass_entry = ctk.CTkEntry(root)
 
-        bt4 = ctk.CTkButton(root , text = "Change Password",font=("Times New Roman", 16),text_color="#CCF5AC" , command = lambda : [change_pass1()])
+    bt5 = ctk.CTkButton(root, text="Change",font=("Times New Roman", 16),text_color="#CCF5AC",command=change_pass2)
 
-        bt4.place(x= 100 , y = 250)
+    Label_backup_done = ctk.CTkLabel(root,font=("Times New Roman", 16),text="Backup done !!!")
 
-        ## ENTRY TO GET NEW PASS
-        new_pass_entry = ctk.CTkEntry(root)
+    for b in (bt1, bt3, bt4, bt5):
+        add_hover_effects(b)
 
-        bt5 = ctk.CTkButton(root , text = "Change" ,font=("Times New Roman", 16),text_color="#CCF5AC", command = lambda : [change_pass2()])
-
-        ## LABEL TO SHOW THAT PASSWORD IS CHANGED
-        Label_backup_done = ctk.CTkLabel(root,font=("Times New Roman", 16), text = "Backup done !!!")
-        
-
-        ## ADDING HOVER EFFECT TO EVERY BUTTON
-        add_hover_effects(bt1)
-        add_hover_effects(bt2)
-        add_hover_effects(bt3)
-        add_hover_effects(bt4)
-        add_hover_effects(bt5)
-
-        ## RUN THE MAIN WINDOW
-        root.mainloop()
+    root.mainloop()
 
 main()
--- main_diary.py
+++ main_diary.py
@@ -1,105 +1,110 @@
 import os
+import sys
 import customtkinter as ctk
 from tkinter import scrolledtext
 import calendar
 from datetime import datetime
+import encrypter as enc
+from tkinter import messagebox
 from tkextrafont import Font
 
-_january_font = None
 
-def get_january_font():
-    global _january_font
-    if _january_font is None:
-        _january_font = Font(
-            file="fonts\\January Night.ttf",
-            family="January Night",
-            size=15
-        )
-    return _january_font
+APP_NAME = "diary_planner"
 
-_abasalom = None
+def get_data_dir():
+    base = os.getenv("APPDATA")
+    path = os.path.join(base, APP_NAME)
+    os.makedirs(path, exist_ok=True)
+    return path
 
-def get_abasalom_font():
-    global _abasalom
-    if _abasalom is None:
-        _abasalom = Font(file="fonts\\new\\Abasalomdemo-ax8Am.otf" )
-    return _abasalom
+DATA_DIR = get_data_dir()
+DIARY_DIR = os.path.join(DATA_DIR, "diary_entries")
+os.makedirs(DIARY_DIR, exist_ok=True)
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
-file_location = "data/diary_entries"
-if not os.path.exists(file_location):
-    os.makedirs(file_location)
-
 current_date = datetime.now()
 selected_date = current_date
 
-def add_hover_effects(button, hover_color="#78C3FB", normal_color="#1F538D" , Text_colour_hover = "#303633" , normal_text = "#CCF5AC"):
+
+def add_hover_effects(button, hover_color="#78C3FB", normal_color="#1F538D",
+                      Text_colour_hover="#303633", normal_text="#CCF5AC"):
     def on_hover(event):
-        button.configure(fg_color=hover_color , text_color= Text_colour_hover)
+        button.configure(fg_color=hover_color, text_color=Text_colour_hover)
 
     def on_leave(event):
-        button.configure(fg_color=normal_color, text_color = normal_text)
+        button.configure(fg_color=normal_color, text_color=normal_text)
 
     button.bind("<Enter>", on_hover)
     button.bind("<Leave>", on_leave)
 
 def ency(s):
-    s_2 = ""
-
-    for i in s:
-        x = ord(i)
-        x = x+4
-        s_2 += chr(x)
-
-    return s_2
+    return "".join(chr(ord(i) + 4) for i in s)
 
 def decy(s):
-    s_2 = ""
-    for i in s:
-        x = ord(i)
-        x-=4
-        s_2 += chr(x)
-    return s_2
+    return "".join(chr(ord(i) - 4) for i in s)
+
 
 def create_calendar(root, calendar_frame, text_area, date_label, year, month):
     for widget in calendar_frame.winfo_children():
         widget.destroy()
 
-    # Create the navigation buttons (Previous and Next month)
-    prev_month_button = ctk.CTkButton(calendar_frame, text="<<", width=30, height=30, font=("Arial", 16, "bold"),
-                                      fg_color="#0E5CFF", text_color="#CCF5AC", command=lambda: change_month(root, calendar_frame, text_area, date_label, year, month - 1))
+    prev_month_button = ctk.CTkButton(
+        calendar_frame, text="<<", width=30, height=30,
+        font=("Arial", 16, "bold"),
+        fg_color="#0E5CFF", text_color="#CCF5AC",
+        command=lambda: change_month(root, calendar_frame, text_area, date_label, year, month - 1)
+    )
     prev_month_button.grid(row=0, column=0, padx=5, pady=5)
 
-    next_month_button = ctk.CTkButton(calendar_frame, text=">>", width=30, height=30, font=("Arial", 16, "bold"),
-                                      fg_color="#0E5CFF", text_color="#CCF5AC", command=lambda: change_month(root, calendar_frame, text_area, date_label, year, month + 1))
+    next_month_button = ctk.CTkButton(
+        calendar_frame, text=">>", width=30, height=30,
+        font=("Arial", 16, "bold"),
+        fg_color="#0E5CFF", text_color="#CCF5AC",
+        command=lambda: change_month(root, calendar_frame, text_area, date_label, year, month + 1)
+    )
     next_month_button.grid(row=0, column=6, padx=5, pady=5)
 
-    # Adjust the position of the month-year label
-    month_year_label = ctk.CTkLabel(calendar_frame, text=f"{calendar.month_name[month]} {year}", font=font_month, text_color="#7FB069")
+    month_year_label = ctk.CTkLabel(
+        calendar_frame,
+        text=f"{calendar.month_name[month]} {year}",
+        font=font_month,
+        text_color="#7FB069"
+    )
     month_year_label.grid(row=0, column=1, columnspan=5, pady=10)
 
     days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     for i, day in enumerate(days):
-        ctk.CTkLabel(calendar_frame, text=day, text_color="#7FB069", font=font_week ).grid(row=1, column=i, padx=5, pady=5)
+        ctk.CTkLabel(calendar_frame, text=day,
+                     text_color="#7FB069",
+                     font=font_week).grid(row=1, column=i, padx=5, pady=5)
 
-    cal = calendar.monthcalendar(year, month)  # Get the month view (cal is a list of weeks)
+    cal = calendar.monthcalendar(year, month)
     for week_num, week in enumerate(cal):
         for day_num, day in enumerate(week):
             if day != 0:
-                # Modify the button to pass the correct year and month
-                btn = ctk.CTkButton(calendar_frame, text=str(day), width=30, height=30, font=("Times New Roman", 16, "bold"), fg_color="#0E5CFF", text_color="#CCF5AC",
-                                    command=lambda d=day, y=year, m=month: on_date_click(root, calendar_frame, text_area, date_label, d, y, m))
+                btn = ctk.CTkButton(
+                    calendar_frame,
+                    text=str(day),
+                    width=30,
+                    height=30,
+                    font=("Times New Roman", 16, "bold"),
+                    fg_color="#0E5CFF",
+                    text_color="#CCF5AC",
+                    command=lambda d=day, y=year, m=month:
+                        on_date_click(root, calendar_frame, text_area, date_label, d, y, m)
+                )
                 add_hover_effects(btn)
                 btn.grid(row=week_num + 2, column=day_num, padx=5, pady=5)
-                if day == selected_date.day and month == selected_date.month and year == selected_date.year:
+
+                if (day == selected_date.day and
+                        month == selected_date.month and
+                        year == selected_date.year):
                     btn.configure(fg_color="#1F6AA5")
 
-
-
 def change_month(root, calendar_frame, text_area, date_label, year, month):
-    # Update the year and month if it's valid
     if month < 1:
         month = 12
         year -= 1
@@ -107,82 +112,70 @@ def change_month(root, calendar_frame, text_area, date_label, year, month):
         month = 1
         year += 1
 
-    # Call create_calendar with the new year and month
     create_calendar(root, calendar_frame, text_area, date_label, year, month)
-
-
 
 def on_date_click(root, calendar_frame, text_area, date_label, day, year, month):
     global selected_date
     save_entry(text_area)
     text_area.after(100, text_area.focus_set)
-    selected_date = datetime(year, month, day)  # Use the passed year and month here
+    selected_date = datetime(year, month, day)
     date_label.configure(text=f"Writing for: {selected_date.strftime('%Y-%m-%d')}")
-    create_calendar(root, calendar_frame, text_area, date_label, year, month)  # Recreate calendar with new date
+    create_calendar(root, calendar_frame, text_area, date_label, year, month)
     load_entry(text_area)
 
 
 def load_entry(text_area):
-    file_path = os.path.join(file_location, selected_date.strftime("%Y-%m-%d.txt"))
+    file_path = os.path.join(DIARY_DIR, selected_date.strftime("%Y-%m-%d.txt"))
     text_area.delete(1.0, ctk.END)
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             text_area.insert(ctk.INSERT, decy(file.read().lstrip()))
 
 def save_entry(text_area):
-    file_path = os.path.join(file_location, selected_date.strftime("%Y-%m-%d.txt"))
-    
-    text_content = text_area.get(1.0, ctk.END).strip()  
-    
-    if text_content:  
+    file_path = os.path.join(DIARY_DIR, selected_date.strftime("%Y-%m-%d.txt"))
+    text_content = text_area.get(1.0, ctk.END).strip()
+
+    if text_content:
         with open(file_path, "w") as file:
             file.write(ency(text_content))
-    else:  
+    else:
         if os.path.exists(file_path):
             os.remove(file_path)
-
-
 
 def auto_save(event, text_area):
     save_entry(text_area)
 
 def main():
-    global text_area , save_button , date_label , font_month , font_week
+    global text_area, save_button, date_label, font_month, font_week
+
     root = ctk.CTk()
     root.title("Daily Diary")
     root.geometry("1200x600+88+50")
-    root.resizable(False , False)
-    root.configure(bg_color = "#303633")
-    root.iconbitmap("icon.ico")
-        
+    root.resizable(False, False)
+    root.configure(bg_color="#303633")
 
-    calendar_frame = ctk.CTkFrame(root, width=200, corner_radius=10 , bg_color="#303633")
-    calendar_frame.place(x = 10,y = 10)
+    calendar_frame = ctk.CTkFrame(root, width=200, corner_radius=10, bg_color="#303633")
+    calendar_frame.place(x=10, y=10)
 
+    font_month = ctk.CTkFont(family="Times new Roman", size=30, weight="bold", underline=True)
+    font_week = ctk.CTkFont(family="Times new Roman", size=15, underline=True)
+    font_date = ctk.CTkFont(family="Times new Roman", size=20, slant="italic")
 
-    font_textarea = get_january_font()
-    font_temp_1 = get_abasalom_font()
-    family1 ="Abasalom Demo"
-    font_month = ctk.CTkFont(family=family1 , size = 30 , weight="bold" , underline=True)
-    font_week = ctk.CTkFont(family=family1 , size = 15 , underline=True)
-    font_date = ctk.CTkFont(family=family1 , size = 20 ,slant="italic")
+    text_area = scrolledtext.ScrolledText(root, wrap=ctk.WORD, width=85, height=12,bg="#2E2E2E", fg="#7FB069",font=("Times new Roman" ,15),spacing1=10, spacing2=5,spacing3=10)
+    text_area.place(x=298, y=30)
 
-    text_area = scrolledtext.ScrolledText(root, wrap=ctk.WORD, width=97, height=14, bg="#2E2E2E", fg="#7FB069", font=font_textarea, spacing1=10, spacing2=5, spacing3=10)
-    text_area.place(x = 298 , y = 30)
-
-    date_label = ctk.CTkLabel(root, text=f"Writing for: {selected_date.strftime('%d-%m-%y')}", font=font_date, text_color="#b0cac7")
-    date_label.place(x = 300 , y = 0)
+    date_label = ctk.CTkLabel(root,text=f"Writing for: {selected_date.strftime('%d-%m-%y')}",font=font_date,text_color="#b0cac7")
+    date_label.place(x=300, y=0)
 
     save_button = ctk.CTkButton(root, text="Save", command=lambda: save_entry(text_area))
-    save_button.place(x= 298 , y = 570)
-
+    save_button.place(x=298, y=570)
     add_hover_effects(save_button)
 
-    create_calendar(root, calendar_frame, text_area, date_label, current_date.year, current_date.month)
+    create_calendar(root, calendar_frame, text_area, date_label,
+                    current_date.year, current_date.month)
     load_entry(text_area)
 
     text_area.bind("<KeyRelease>", lambda event: auto_save(event, text_area))
-
     text_area.after(100, text_area.focus_set)
 
     root.mainloop()
